8501796dbbedca3b0c09edc5d93351e953af4671
grishazmeypro <grisha@telia.com>
2014-05-22 15:46:17 +0200

    try

--- test/ImageControllerTest.php
+++ test/ImageControllerTest.php
@@ -6,27 +6,23 @@ class ImageControllerTest extends \PHPUnit_Framework_TestCase
 	public function OutputImageTest()
 	{
 		require('../Image/ImageController.php');
-		$path = CACHE_PATH;
 		define('IMG_PATH' ,__DIR__ .'imgtest/');
-		define('CACHE_PATH' , __DIR__ . 'cache/');
+		define('CACHE_PATH' , __DIR__ );
 		$link = 'test.jpg';
 		$height = 240;
 		$width = 300;
 		$object = new Anax\Image\ImageController();
 		$object->showAction($link,$width,$height);
-		$files = scandir($path);
-		if(is_array($files))
+		$imgInfo = getimagesize(CACHE_PATH.'-.-test_240_300_q60.jpg');
+		if(($imgInfo[0] != 300)&&($imgInfo[0] != 240))
 		{
-			if(isset($files[0]))
-			{
-				$imgInfo = getimagesize($files[0]);
-				$this->pass();
-			}
-			else
-			{
-				$this->fail();
-			}
+			$this->fail();
 		}
+		else
+		{
+			$this->pass();
+		}
+		
 		
 	}
 	
