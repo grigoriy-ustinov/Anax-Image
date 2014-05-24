e2c8666ff76f7939fa64b7a192c5dfca8b3ede6f
grishazmeypro <grisha@telia.com>
2014-05-24 19:28:52 +0200

    33

--- test/Image/ImageControllerTest.php
+++ test/Image/ImageControllerTest.php
@@ -8,12 +8,14 @@ class ImageControllerTest extends \PHPUnit_Framework_TestCase
 		
 		define('IMG_PATH' ,__DIR__ .DIRECTORY_SEPARATOR.'imgtest'. DIRECTORY_SEPARATOR);
 		define('CACHE_PATH' , __DIR__ .DIRECTORY_SEPARATOR);
-		$link = 'test.jpg';
 		$height = 240;
 		$width = 300;
 		$flag = false;
 		$filename = "test.jpg";
-
+		
+		chmod(IMG_PATH,0777);
+		chmod(CACHE_PATH,0777);
+		
 		$image = new \Anax\Image\Image($filename,$width,$height);		
 		$image->validate();
 		$image->getImageInfo();
