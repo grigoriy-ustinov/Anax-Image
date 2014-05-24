a0c38e67657438e8f159b72c432b068022708cca
grishazmeypro <grisha@telia.com>
2014-05-24 19:05:13 +0200

    chmod

--- test/Image/ImageControllerTest.php
+++ test/Image/ImageControllerTest.php
@@ -13,9 +13,10 @@ class ImageControllerTest extends \PHPUnit_Framework_TestCase
 		$width = 300;
 		$flag = false;
 		$filename = "test.jpg";
-		
+		chmod(IMG_PATH,0777);
+		chmod(CACHE_PATH,0777);
 		$image = new \Anax\Image\Image($filename,$width,$height);		
-		//$image->validate();
+		$image->validate();
 		$image->getImageInfo();
 		$image->createFilenameForCache();
 		$image->openOriginalImage();
